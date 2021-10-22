c06ad89c862d457bb9cb94b192b6592fe1e7da53
Saurabh <saurabh.d2106@gmail.com>
2021-10-22 13:07:42 +0530

    Amazon Assignment

---
+++ cypress/integration/amazonProject/SearchProduct_spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe ("Verify search Product test cases", function() {
+
+    beforeEach(()=>{
+        cy.visit("https://amazon.in")
+    })
+
+    it("Verify Search Product Test", function() {
+      
+        cy.get("#searchDropdownBox").scrollIntoView().select("Electronics", {force: true})
+
+        cy.get("#twotabsearchtextbox").type("Apple Watch")
+
+        cy.get("#nav-search-submit-button").click()
+
+        cy.get("div[data-component-type='s-search-result']").as('allProductItems')
+
+        cy.get("@allProductItems").should("have.length", 27)
+
+        cy.get("@allProductItems").eq(5).invoke('text').then(productDetails => {
+            expect(productDetails).to.contain("Apple Watch")
+        })
+
+        cy.get("@allProductItems").eq(5).invoke('text').then(productDetails => {
+            cy.log(productDetails)
+        })
+
+        cy.get("@allProductItems").each(($el, index, $list) => {
+            
+            cy.log("Index : " + index + " and the product details are - "+ $el.text())
+
+        })
+
+        cy.get("@allProductItems").eq(5).click()
+
+
+    })
+
+    
+
+    
+
+})
